8edd54446ea17d22414a1fadf5e144cef209a14d
Ilham T. Arfian <ilhamta27@gmail.com>
2022-06-01 21:42:26 +0700

    control flow exercise

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -20,7 +20,13 @@ fn main() {
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
-
+        if arg.eq("sum") {
+            sum()
+        } else if arg.eq("double") {
+            double()
+        } else {
+            count(String::from("arg"))
+        }
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
     }
@@ -32,7 +38,9 @@ fn sum() {
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
 
-
+    for i in 7..=23 {
+        sum += i;
+    }
     println!("The sum is {}", sum);
 }
 
@@ -43,7 +51,10 @@ fn double() {
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
-
+    while x < 500 {
+        x = x * 2;
+        count += 1;
+    }
     println!("You can double x {} times until x is larger than 500", count);
 }
 
@@ -51,8 +62,14 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
-
+    // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+    let mut counter: i32 = 0;
+    loop {
+        print!("{} ", arg);
+        counter += 1;
+        if counter == 8 {
+            break;
+        }
+    }
     println!(); // This will output just a newline at the end for cleanliness.
 }
